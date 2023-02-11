270ea7a2839bba7d85cfe3d141f330eac709ac7c
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2023-02-11 10:08:42 +0900

    白黒画

--- Unit1.pas
+++ Unit1.pas
@@ -9,20 +9,18 @@ uses
 type
   TForm1 = class(TForm)
     Image1: TImage;
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
     procedure Image1Click(Sender: TObject);
   private
     FGPBitmap: array[1..2] of TGPBitmap;
+    FFlg: Boolean;
     procedure Image1Paint(Count: Integer);
   public
     { Public 宣言 }
   end;
 
 const
-  FILE1 = 'z:\file1.jpg';
-  FILE2 = 'z:\file2.jpg';
-  WAITTIME = 10; //1回ごとのウェイトタイム（ミリ秒）
+  FILE1 = 'z:\file2.jpg';
+  WAITTIME = 20; //1回ごとのウェイトタイム（ミリ秒）
   TIMES = 100;   //変身までに何回描画するか
 
 var
@@ -32,18 +30,6 @@ implementation
 
 {$R *.dfm}
 
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  FGPBitmap[1] := TGPBitmap.Create(FILE1);
-  FGPBitmap[2] := TGPBitmap.Create(FILE2);
-end;
-
-procedure TForm1.FormDestroy(Sender: TObject);
-begin
-  FreeAndNil(FGPBitmap[1]);
-  FreeAndNil(FGPBitmap[2]);
-end;
-
 procedure TForm1.Image1Click(Sender: TObject);
 begin
   for var i := 0 to TIMES do begin
@@ -51,34 +37,33 @@ begin
     Image1.Repaint;
     Sleep(WAITTIME);
   end;
-  var swap := FGPBitmap[1];
-  FGPBitmap[1] := FGPBitmap[2];
-  FGPBitmap[2] := swap;
+  FFlg := not FFlg;
 end;
 
 procedure TForm1.Image1Paint(Count: Integer);
 const
   CM_DEFAULT : TColorMatrix = (
-      (1, 0, 0, 0, 0),
-      (0, 1, 0, 0, 0),
-      (0, 0, 1, 0, 0),
-      (0, 0, 0, 9, 0),
+      (1/3, 1/3, 1/3, 0, 0),
+      (1/3, 1/3, 1/3, 0, 0),
+      (1/3, 1/3, 1/3, 0, 0),
+      (0, 0, 0, 1, 0),
       (0, 0, 0, 0, 1));
 var
   gcanvas: TGPGraphics;
   cm: TColorMatrix;
   ia: TGPImageAttributes;
 begin
+
+  FGPBitmap[1] := TGPBitmap.Create(FILE1);
+  FGPBitmap[2] := TGPBitmap.Create(FILE1);
+
   var w := FGPBitmap[1].GetWidth;
   var h := FGPBitmap[1].GetHeight;
-
   ia := TGPImageAttributes.Create;
-  gcanvas := TGPGraphics.Create(Image1.Canvas.Handle);
-
   cm := CM_DEFAULT;
 
-  cm[3,3] := Count / TIMES;
   ia.SetColorMatrix(cm);
+  gcanvas := TGPGraphics.Create(FGPBitmap[2]);
   gcanvas.DrawImage(FGPBitmap[1],
                     MakeRect(0, 0, w, h),
                     0, 0,
@@ -86,8 +71,11 @@ begin
                     UnitPixel,
                     ia);
 
-  cm[3,3] := 1 - cm[3,3];
-  ia.SetColorMatrix(cm);
+  if FFlg then ia.SetThreshold(1- Count/TIMES)
+          else ia.SetThreshold(Count/TIMES);
+
+  FreeAndNil(gcanvas);
+  gcanvas := TGPGraphics.Create(Image1.Canvas.Handle);
   gcanvas.DrawImage(FGPBitmap[2],
                     MakeRect(0, 0, w, h),
                     0, 0,
@@ -95,8 +83,12 @@ begin
                     UnitPixel,
                     ia);
 
+
   FreeAndNil(gcanvas);
   FreeAndNil(ia);
+
+  FreeAndNil(FGPBitmap[1]);
+  FreeAndNil(FGPBitmap[2]);
 end;
 
 end.
